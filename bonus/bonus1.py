0263665d707057a6c0c53680beccffe91c00e633
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-03 22:48:05 +0530

    3 rd day completed

---
+++ bonus/bonus1.py
@@ -0,0 +1,9 @@
+country = "India"
+
+match country:
+    case "India":
+        print("Namasthe")
+    case "USA":
+        print("Hello")
+    case "germany":
+        print("Hi")
---
+++ bonus/bonus2.py
@@ -0,0 +1,4 @@
+members = ["john smith", "sen plakay", "dora ngacely"]
+
+for member in members:
+    print(member.title())
