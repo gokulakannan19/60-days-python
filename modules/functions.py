67501c73f50d286332c18e4d84a158d472216300
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-16 21:52:03 +0530

    day 14 completed

---
+++ modules/functions.py
@@ -0,0 +1,12 @@
+def get_todos(filepath="files/todos.txt"):
+    """" Read the text file and return the todo items"""
+    with open(filepath) as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath="files/todos.txt"):
+    """ Write the todo items list to the text file"""
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
+
