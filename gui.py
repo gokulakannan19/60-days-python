87ba259543247cd0039a8050ada94a4024367266
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-21 13:04:06 +0530

    implemented edit functionality day 17

--- gui.py
+++ gui.py
@@ -4,15 +4,20 @@ from modules import functions
 label = sg.Text("Type in a Todo")
 input_text = sg.InputText(tooltip="Enter a Todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=(45, 10))
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_text, add_button]],
+                   layout=[[label], [input_text, add_button],
+                           [list_box, edit_button]],
                    font=("Helvitica", 20))
 
 while True:
     event, values = window.read()
     print(event)
     print(values)
+    print(values["todos"])
 
     match event:
         case "Add":
@@ -20,6 +25,21 @@ while True:
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window["todos"].update(values=functions.get_todos())
+
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"] + "\n"
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window["todos"].update(values=functions.get_todos())
+
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
+
         case sg.WIN_CLOSED:
             break
 
