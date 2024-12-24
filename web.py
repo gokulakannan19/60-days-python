79e56a047b3dbc5c099a7620705532476cc44c68
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-24 21:54:49 +0530

    implemented add functionality streamlit day19

--- web.py
+++ web.py
@@ -4,6 +4,12 @@ from modules import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo_local = st.session_state["new_todo"] + "\n"
+    todos.append(todo_local)
+    functions.write_todos(todos)
+
 st.title("My Todo App")
 st.subheader("This is a todo app to gain knowledge")
 st.write("It will boost your performance")
@@ -11,4 +17,7 @@ st.write("It will boost your performance")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new Todo...")
+st.text_input(label=" ", placeholder="Add a new Todo...",
+              on_change=add_todo, key="new_todo")
+
+st.session_state
